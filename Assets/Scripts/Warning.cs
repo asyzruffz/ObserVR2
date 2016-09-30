4ce2dabbef1592312ee84572c9ea56827faa368d
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-09-30 14:49:21 +0800

    warning unfinished yet

---
+++ Assets/Scripts/Warning.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warning : MonoBehaviour {
+
+    public Camera cam;
+
+	void Start ()
+    {
+	    
+	}
+    
+	void Update ()
+    {
+        var meteoroids = FindObjectsOfType<Meteoroid>();
+
+        if(meteoroids.Length > 0)
+        {
+            //Debug.Log(cam.WorldToViewportPoint(meteoroids[0].gameObject.transform.position).ToString());
+            Vector3 viewportPos = cam.WorldToViewportPoint(meteoroids[0].gameObject.transform.position);
+            float screenX = Mathf.Clamp(viewportPos.x, 0, 1);
+            float screenY = Mathf.Clamp(viewportPos.y, 0, 1);
+            transform.localPosition = new Vector3(screenX, screenY);
+        }
+	}
+}
