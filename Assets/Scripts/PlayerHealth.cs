8c459fe3013ca81e393dd351a4a7aa6071fa0bd5
Tan Kah Loon <kahloontan@gmail.com>
2016-09-30 14:42:59 +0800

    auto restart

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
 
 	public GameObject player;
+	public GameObject restartText;
 	public int lives = 4;
+	MeshRenderer textRender;
 
 	public Vector3 textPos; //0,-12,20
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		restartText = GameObject.Find ("Restart");
+		textRender = restartText.GetComponent<MeshRenderer> ();
 
 		this.transform.position = player.transform.position+textPos;
 	}
@@ -17,6 +22,20 @@ public class PlayerHealth : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+
+
+		if (lives == 0) {
+			textRender.enabled = true;
+
+			Invoke ("restart", 5.0f);
+			//Time.timeScale = 0.0f;
+		} else {
 			GetComponent<TextMesh> ().text = "Lives: " + lives;
+		}
+
+	}
+
+	void restart(){
+		SceneManager.LoadScene ("Game");
 	}
 }
