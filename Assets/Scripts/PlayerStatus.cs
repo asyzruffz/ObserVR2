6e2c89416f5904d33a70549a84a010fd7d3e0a75
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-10-06 05:59:10 +0800

    meteor warning sound

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class PlayerStatus : MonoBehaviour {
 
     public ImageFade blood;
+    public float warningDistance;
 
     public delegate void HitDelegate();
     public event HitDelegate hitEvent;
 
     AudioSource painSound;
     AudioSource deathSound;
+    AudioSource warningSound;
+
+    private bool warning = false;
+    private bool warnSounding = false;
 
     void Start () {
         hitEvent += OnHit;
@@ -18,11 +23,31 @@ public class PlayerStatus : MonoBehaviour {
         AudioSource[] aSource = GetComponents<AudioSource>();
         painSound = aSource[0]; //will give error but still working
         deathSound = aSource[1];
+        warningSound = aSource[2];
     }
 	
 	void Update () {
-	
-	}
+        warning = false;
+        var meteoroids = FindObjectsOfType<Meteoroid>();
+        foreach(var meteoroid in meteoroids)
+        {
+            if(meteoroid != null)
+            {
+                float distance = Vector3.Distance(transform.position, meteoroid.transform.position);
+                if(distance < warningDistance)
+                {
+                    warning = true;
+                    break;
+                }
+            }
+        }
+
+        if(warning && !warnSounding)
+        {
+            warnSounding = true;
+            AudioSource.PlayClipAtPoint(warningSound.clip, transform.position);
+        }
+    }
 
     public void Hit()
     {
