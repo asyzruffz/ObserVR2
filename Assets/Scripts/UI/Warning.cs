adc72370c382dbbefe288ebd71226e6976fb1989
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-10-07 01:38:41 +0800

    indicator changes from arrowhead to exlamation mark

--- Assets/Scripts/UI/Warning.cs
+++ Assets/Scripts/UI/Warning.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,8 +9,11 @@ public class Warning : MonoBehaviour {
     public GameObject indicator;
     public float radiusBorder = 180f;
     public bool rotateTowards = true;
+    public Sprite warningSprite;
 
-    private int num = 5; 
+    private int num = 5;
+    private float warningDistance;
+    private Sprite origSprite;
     private List<GameObject> instances = new List<GameObject>();
 
 	void Start ()
@@ -23,6 +27,9 @@ public class Warning : MonoBehaviour {
             inst.SetActive(false);
             instances.Add(inst);
         }
+
+        origSprite = instances[0].GetComponent<Image>().sprite;
+        warningDistance = FindObjectOfType<PlayerStatus>().warningDistance;
     }
     
 	void Update ()
@@ -34,7 +41,7 @@ public class Warning : MonoBehaviour {
             {
                 instances[i].SetActive(true);
 
-                Vector3 viewportPos = cam.WorldToViewportPoint(meteoroids[i].gameObject.transform.position);
+                Vector3 viewportPos = cam.WorldToViewportPoint(meteoroids[i].transform.position);
                 float screenX = Mathf.Clamp(viewportPos.x, 0, 1) * 2 - 1;
                 float screenY = Mathf.Clamp(viewportPos.y, 0, 1) * 2 - 1;
 
@@ -51,9 +58,21 @@ public class Warning : MonoBehaviour {
                     screenY = adjust.normalized.y;
                 }
 
-                if (rotateTowards)
+                bool warn = Vector3.Distance(transform.position, meteoroids[i].transform.position) < warningDistance * 2;
+
+                if (rotateTowards && !warn)
                 {
                     instances[i].transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(screenY, screenX) * Mathf.Rad2Deg);
+                }
+
+                if(warn)
+                {
+                    instances[i].GetComponent<Image>().sprite = warningSprite;
+                    instances[i].transform.localEulerAngles = Vector3.zero;
+                }
+                else
+                {
+                    instances[i].GetComponent<Image>().sprite = origSprite;
                 }
 
                 instances[i].transform.localPosition = new Vector3(screenX * radiusBorder, screenY * radiusBorder, transform.localPosition.z);
