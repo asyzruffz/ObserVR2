6c38ab87458216e8279335be625868c1efc17c96
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-11-26 20:30:45 +0800

    Added hideable component property

--- Assets/Scripts/MeteorManager.cs
+++ Assets/Scripts/MeteorManager.cs
@@ -3,34 +3,33 @@ using System.Collections;
 
 public class MeteorManager : MonoBehaviour {
 
+	[Header("Meteor")]
+	public GameObject meteorPrefab;
 	public float spawnTime; //Time until next meteor spawned
 	public float meteorLimit; //to limit number of meteor flying towrds player
-	public GameObject meteor; //for the static variable of meteorCounter
-	public static int meteorCounter; //Collide.cs will use it too
-	public FauxGravityAttractor player; //gravity attractor purpose
 
+	//for the static variable of meteorCounter
+	public static int meteorCounter; //Collide.cs will use it too
+
+	[Header("Location")]
+	public FauxGravityAttractor player; //gravity attractor purpose
 	public float radius;  //the radius of meteor will spawned
     [Range(0,180)]
     public float angleRange = 180f;
 
+	[Header("Gravity")]
 	public float gravityChangeInterval; //the time until gravity is increased
 	public float gravityChange; //the speed of gravity increased
     
 	void Start () {
-        if(player == null)
-        {
+        if(player == null) {
             player = FindObjectOfType<FauxGravityAttractor>();
         }
         Director.Instance.startEvent += OnStartGame;
         Director.Instance.endEvent += OnStopGame;
     }
-	
-	void Update () {
-        
-	}
 
-    void OnStartGame()
-    {
+    void OnStartGame() {
         meteorCounter = 0;
         player.gravity = 0.1f;
 
@@ -38,15 +37,13 @@ public class MeteorManager : MonoBehaviour {
         Invoke("gravity", gravityChangeInterval + 3);
     }
 
-    void OnStopGame()
-    {
+    void OnStopGame() {
         CancelInvoke("spawn");
         CancelInvoke("gravity");
     }
 
     //spawn meteor
-    void spawn()
-	{
+    void spawn() {
 		if (meteorCounter < meteorLimit) {
 
             Vector3 meteorPos;
@@ -54,7 +51,7 @@ public class MeteorManager : MonoBehaviour {
                 meteorPos = Random.onUnitSphere * radius;
             } while (Mathf.Abs(meteorPos.y) > radius * Mathf.Sin(angleRange * 0.5f * Mathf.Deg2Rad));
 
-            GameObject meteorObject = (GameObject)Instantiate(meteor, meteorPos, Quaternion.identity);
+            GameObject meteorObject = (GameObject)Instantiate(meteorPrefab, meteorPos, Quaternion.identity);
             meteorObject.name = "Meteor";
             meteorObject.transform.parent = gameObject.transform;
             meteorCounter++;
@@ -64,8 +61,7 @@ public class MeteorManager : MonoBehaviour {
     }
 
 	//change gravity
-	void gravity()
-	{
+	void gravity() {
 		player.gravity += gravityChange;
         Invoke("gravity", gravityChangeInterval);
     }
