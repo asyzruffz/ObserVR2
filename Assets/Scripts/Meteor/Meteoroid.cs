01d0e46e2cda3e16bde61d9859bcbfdade6a83dc
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2016-12-01 01:00:41 +0800

    Meteor is burning

--- Assets/Scripts/Meteor/Meteoroid.cs
+++ Assets/Scripts/Meteor/Meteoroid.cs
@@ -4,32 +4,42 @@ using System.Collections;
 public class Meteoroid : MonoBehaviour {
 
     public GameObject destroyEffect;
+    //public GameObject trailEffect;
     public GameObject[] variations;
     public float hardness = 100;
 
     private Selectable select;
     private bool exploded = false;
     private AudioSource explodeSound;
+    private float maxHardness;
 
 	void Awake () {
         int rng = Random.Range(0, variations.Length);
-        GameObject model = (GameObject)Instantiate(variations[rng], transform.position, Quaternion.identity);
+        GameObject model = (GameObject)Instantiate(variations[rng], transform.position, transform.rotation);
         model.transform.localScale = transform.localScale;
         model.transform.parent = transform;
         model.name = "Model";
-	}
+
+        /*GameObject trail = (GameObject)Instantiate(trailEffect, transform.position, transform.rotation);
+        trail.transform.parent = transform;
+        trail.transform.localScale = Vector3.one;
+        trail.name = "Burning";*/
+    }
 
     void Start() {
         select = GetComponent<Selectable>();
         explodeSound = GetComponent<AudioSource>();
         Director.Instance.endEvent += OnExplode;
         FindObjectOfType<PlayerStatus>().hitEvent += OnHitPlayer;
+        maxHardness = hardness;
     }
 	
 	void Update () {
 	    if(select.connected) {
             hardness -= DamagePerSecond() * Time.deltaTime;
         }
+
+        GetComponentInChildren<BurningMeteoroid>().burntRate = 1 - (hardness / maxHardness);
 
         if(hardness <= 0 && !exploded) {
             OnExplode();
