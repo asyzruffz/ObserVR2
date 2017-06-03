5ad8dd31ef461a93ac51b69046d624c9d60c1a24
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2017-06-03 13:56:48 +0800

    fov editor script changes

--- Assets/Editor/FieldOfViewEditor.cs
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -9,12 +9,18 @@ public class FieldOfViewEditor : Editor {
     {
         FieldOfView fov = (FieldOfView)target;
         Handles.color = Color.green;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
 
-        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
-        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+        float halfAngle = fov.viewAngle / 2;
+
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, fov.DirFromAngle (-halfAngle, false), fov.viewAngle, fov.viewRadius);
+        Handles.DrawWireArc(fov.transform.position + fov.transform.forward * Mathf.Cos (halfAngle * Mathf.Deg2Rad) * fov.viewRadius, -fov.transform.forward, -fov.transform.right, 180, Mathf.Sin (halfAngle * Mathf.Deg2Rad) * fov.viewRadius);
+
+        Vector3 viewAngleLeft = fov.DirFromAngle(-halfAngle, false);
+        Vector3 viewAngleRight = fov.DirFromAngle(halfAngle, false);
+        Vector3 viewAngleTop = Quaternion.AngleAxis (-halfAngle, fov.transform.right) * fov.transform.forward;
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleLeft * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleRight * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleTop * fov.viewRadius);
 
         Handles.color = Color.red;
         foreach(Transform visibleTarget in fov.visibleTargets)
